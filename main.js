eb86928b66d6e2302ddcb4228f3ef529e9ad33fd
Reenakhan143 <159185285+Reenakhan143@users.noreply.github.com>
2024-02-29 09:40:24 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// first exercise jis mai hum nay aik object  k andar 
+// HUM NAY STRING number boolean define kara hai
+var student = { name: "muhammad ali",
+    age: 20,
+    isMarried: true };
+console.log(student.age + 4);
