6b0779c85326f974cbe40bb76a936de82b0faf40
Reenakhan143 <159185285+Reenakhan143@users.noreply.github.com>
2024-03-05 17:36:00 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-// first exercise jis mai hum nay aik object  k andar 
-// HUM NAY STRING number boolean define kara hai
-var student = { name: "muhammad ali",
-    age: 20,
-    isMarried: true };
-console.log(student.age + 4);
+// camel case
+var fullName = "Reena khan";
+// snake_case
+var full_name = "muhammad ali";
+// pascal case
+var FullName = "hussain";
+var IsfullName = false;
+var isFullName = false;
+var is_full_name = false;
+console.log(IsfullName);
+console.log(fullName);
+// array
+var student_list = ["reena", "ali", "Hussain", "saba"];
+console.log(student_list[1]);
+// diffrent type array
+var transport = ["bus", 10, "car", 9, true];
+var x = transport[3];
+console.log(transport);
+console.log(" this number is my favouit number," + x);
--- main.ts
+++ main.ts
@@ -1,9 +1,35 @@
-// first exercise jis mai hum nay aik object  k andar 
-// HUM NAY STRING number boolean define kara hai
-let student = {name: "muhammad ali", 
-age: 20,
- isMarried: true};
-console.log(student.age + 4) // 24
+// camel case
+let fullName: string = "Reena khan";
+
+// snake_case
+let full_name:  string = "muhammad ali";
+
+// pascal case
+let FullName: string = "hussain";
+
+let IsfullName:  boolean = false;
+let  isFullName :boolean= false ;
+let  is_full_name:  boolean = false;
+
+console.log(IsfullName);
+console.log(fullName);
+
+// array
+let student_list: string[]= ["reena", "ali","Hussain", "saba"];
+
+console.log( student_list[1]);
+
+// diffrent type array
+let transport: any [] = ["bus", 10, "car",9,true];
+let x = transport [3] as number;
+console.log(transport)
+console.log(" this is number my favourit number," + x );
+
+
+
+
+
+
 
 
 
